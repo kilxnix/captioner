e5e526c24047f4707ccfe122f53965c5c98ce05d
kilxnix <sheltontraylor@gmail.com>
2026-04-19 16:36:57 -0400

    fix: add missing setValue import in HomeScreen

--- app/src/main/java/com/sheltron/captioner/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/sheltron/captioner/ui/screens/HomeScreen.kt
@@ -46,6 +46,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
